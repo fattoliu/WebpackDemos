8edc78b73a0f2bbe416a1b3d6c960627fa0e6e6e
fattoliu <724684054@qq.com>
2018-10-26 14:46:37 +0800

    用 vs code中的 git 提交 代码，测试

--- hello-webpack/b.js
+++ hello-webpack/b.js
@@ -1,5 +1,6 @@
 var msg = "Yo.Yo.check it out!" +
-    "修改了 webpack.config.js的配置文件";
+    "修改了 webpack.config.js的配置文件" +
+    "刚刚把这个 demo 项目 pull 到 github 上了，测试一下";
 var b = 1;
 
 module.exports = {
--- hello-webpack/bundle.js
+++ hello-webpack/bundle.js
@@ -1,111 +1,187 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
 /******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./a.js");
-/******/ })
+(function (modules) { // webpackBootstrap
+  /******/ // The module cache
+  /******/
+  var installedModules = {};
+  /******/
+  /******/ // The require function
+  /******/
+  function __webpack_require__(moduleId) {
+    /******/
+    /******/ // Check if module is in cache
+    /******/
+    if (installedModules[moduleId]) {
+      /******/
+      return installedModules[moduleId].exports;
+      /******/
+    }
+    /******/ // Create a new module (and put it into the cache)
+    /******/
+    var module = installedModules[moduleId] = {
+      /******/
+      i: moduleId,
+      /******/
+      l: false,
+      /******/
+      exports: {}
+      /******/
+    };
+    /******/
+    /******/ // Execute the module function
+    /******/
+    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+    /******/
+    /******/ // Flag the module as loaded
+    /******/
+    module.l = true;
+    /******/
+    /******/ // Return the exports of the module
+    /******/
+    return module.exports;
+    /******/
+  }
+  /******/
+  /******/
+  /******/ // expose the modules object (__webpack_modules__)
+  /******/
+  __webpack_require__.m = modules;
+  /******/
+  /******/ // expose the module cache
+  /******/
+  __webpack_require__.c = installedModules;
+  /******/
+  /******/ // define getter function for harmony exports
+  /******/
+  __webpack_require__.d = function (exports, name, getter) {
+    /******/
+    if (!__webpack_require__.o(exports, name)) {
+      /******/
+      Object.defineProperty(exports, name, {
+        enumerable: true,
+        get: getter
+      });
+      /******/
+    }
+    /******/
+  };
+  /******/
+  /******/ // define __esModule on exports
+  /******/
+  __webpack_require__.r = function (exports) {
+    /******/
+    if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+      /******/
+      Object.defineProperty(exports, Symbol.toStringTag, {
+        value: 'Module'
+      });
+      /******/
+    }
+    /******/
+    Object.defineProperty(exports, '__esModule', {
+      value: true
+    });
+    /******/
+  };
+  /******/
+  /******/ // create a fake namespace object
+  /******/ // mode & 1: value is a module id, require it
+  /******/ // mode & 2: merge all properties of value into the ns
+  /******/ // mode & 4: return value when already ns object
+  /******/ // mode & 8|1: behave like require
+  /******/
+  __webpack_require__.t = function (value, mode) {
+    /******/
+    if (mode & 1) value = __webpack_require__(value);
+    /******/
+    if (mode & 8) return value;
+    /******/
+    if ((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+    /******/
+    var ns = Object.create(null);
+    /******/
+    __webpack_require__.r(ns);
+    /******/
+    Object.defineProperty(ns, 'default', {
+      enumerable: true,
+      value: value
+    });
+    /******/
+    if (mode & 2 && typeof value != 'string')
+      for (var key in value) __webpack_require__.d(ns, key, function (key) {
+        return value[key];
+      }.bind(null, key));
+    /******/
+    return ns;
+    /******/
+  };
+  /******/
+  /******/ // getDefaultExport function for compatibility with non-harmony modules
+  /******/
+  __webpack_require__.n = function (module) {
+    /******/
+    var getter = module && module.__esModule ?
+      /******/
+      function getDefault() {
+        return module['default'];
+      } :
+      /******/
+      function getModuleExports() {
+        return module;
+      };
+    /******/
+    __webpack_require__.d(getter, 'a', getter);
+    /******/
+    return getter;
+    /******/
+  };
+  /******/
+  /******/ // Object.prototype.hasOwnProperty.call
+  /******/
+  __webpack_require__.o = function (object, property) {
+    return Object.prototype.hasOwnProperty.call(object, property);
+  };
+  /******/
+  /******/ // __webpack_public_path__
+  /******/
+  __webpack_require__.p = "";
+  /******/
+  /******/
+  /******/ // Load entry module and return exports
+  /******/
+  return __webpack_require__(__webpack_require__.s = "./a.js");
+  /******/
+})
 /************************************************************************/
-/******/ ({
+/******/
+({
 
-/***/ "./a.js":
-/*!**************!*\
-  !*** ./a.js ***!
-  \**************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+  /***/
+  "./a.js":
+    /*!**************!*\
+      !*** ./a.js ***!
+      \**************/
+    /*! no static exports found */
+    /***/
+    (function (module, exports, __webpack_require__) {
 
-eval("var msgModule = __webpack_require__(/*! ./b */ \"./b.js\");\n\nconsole.log('msg: ', msgModule.msg);\n// alert(msgModule.msg);\n\n//# sourceURL=webpack:///./a.js?");
+      eval("var msgModule = __webpack_require__(/*! ./b */ \"./b.js\");\n\nconsole.log('msg: ', msgModule.msg);\n// alert(msgModule.msg);\n\n//# sourceURL=webpack:///./a.js?");
 
-/***/ }),
+      /***/
+    }),
 
-/***/ "./b.js":
-/*!**************!*\
-  !*** ./b.js ***!
-  \**************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+  /***/
+  "./b.js":
+    /*!**************!*\
+      !*** ./b.js ***!
+      \**************/
+    /*! no static exports found */
+    /***/
+    (function (module, exports) {
 
-eval("var msg = \"Yo.Yo.check it out!\" +\n    \"修改了 webpack.config.js的配置文件\";\nvar b = 1;\n\nmodule.exports = {\n    msg: msg\n};\n\n//# sourceURL=webpack:///./b.js?");
+      eval("var msg = \"Yo.Yo.check it out!\" +\n    \"修改了 webpack.config.js的配置文件\" +\n    \"刚刚把这个 demo 项目 pull 到 github 上了，测试一下\";\nvar b = 1;\n\nmodule.exports = {\n    msg: msg\n};\n\n//# sourceURL=webpack:///./b.js?");
 
-/***/ })
+      /***/
+    })
 
-/******/ });
+  /******/
+});
